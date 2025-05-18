ca496ae5e9d0f8f8e6b79a494630c1bd7d0d0966
ToshifumiDT <toshifumi.digitaletalenter@gmail.com>
2025-05-18 08:11:24 +0200

    reinitialize project and add TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './App.css';
+import './index.css';
 import Header from './components/Header';
 import NoteInput from './components/NoteInput';
 import NoteList from './components/NoteList';
